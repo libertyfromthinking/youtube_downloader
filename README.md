5a26e1ca7dc839d9585bfb544e7f028aba129d49
libertyfromthinking <libertyfromthinking@gmail.com>
2020-05-01 15:03:34 +0900

    modify readme

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # Youtube mp3 Downloader
-===========
-1. 파이썬 환경에서 실행
-2. 1. 재생목록 2. 단일 동영상 선택
-3. mp3 폴더에 다운로드 완료
+
+## 설치방법(우분투 18.04 기준)
+가상환경을 설치합니다
+```sh
+$ python3 -m virtualenv .venv
+```
+
+가상환경을 활성화합니다
+```sh
+$ source .venv/bin/activate
+```
+
+git 저장소를 clone 합니다
+```sh
+$ git clone https://github.com/libertyfromthinking/youtube_downloader.git 
+```
+
+프로젝트 디렉토리로 이동 후 pip를 업데이트 합니다
+```sh
+$ cd django_korean_anagram
+$ pip install -upgrade pip
+```
+
+requirements.txt 파일로 패키지들을 설치합니다
+```sh
+$ pip install -r requirements.txt
+```
+
+ffmpeg를 설치합니다
+```sh
+$ sudo apt install ffmpeg
+```
+
+실행 후 다운받으려는 동영상의 주소를 입력합니다
+```sh
+$ python3 test.py
+```
+
+
+
--- test.py
+++ test.py
@@ -5,19 +5,22 @@ import subprocess
 import moviepy.editor as mp
 import re
 
-# def download_video(addr):
-#     yt = YouTube(addr).streams.filter(mime_type="audio/mp4").first()
-#     tgt_folder = './mp3'
-#     yt.download(tgt_folder)
-#     for file in [n for n in os.listdir(tgt_folder) if re.search('mp4',n)]:
-#         full_path = os.path.join(tgt_folder, file)
-#         output_path = os.path.join(tgt_folder, os.path.splitext(file)[0] + '.mp3')
-#         clip = mp.AudioFileClip(full_path, bitrate='192k').subclip(10,) # disable if do not want any clipping
-#         clip.write_audiofile(output_path)
-#         os.remove(full_path)
 
 def download_video(addr):
-    yt = YouTube(addr).streams.filter(mime_type="audio/mp4").first()
+    '''링크의 동영상을 mp3 형식으로 mp3 폴더에 다운로드합니다
+
+    1. 해당 동영상을 mp4 형식으로 먼저 다운로드 합니다. 재생목록일 경우 1, 단일 동영상일 경우 2 입력
+    2. 동영상 링크를 입력합니다. 축약주소, 모바일주소, 웹주소 모두 가능합니다.
+    3. pytube 라이브러리로 mp3디렉토리에 mp4 파일로 먼저 다운로드받습니다.
+    4. 로컬에 설치되어 있는 ffmpeg로 mp3로 인코딩합니다.
+    5. mp4파일을 삭제합니다.
+    '''
+    try:
+        yt = YouTube(addr).streams.filter(mime_type="audio/mp4").first()
+    except:
+        print('올바른 형식으로 입력해주십시오')
+        return
+
     parent_dir = './mp3'
     yt.download(parent_dir)
 
@@ -36,16 +39,30 @@ def download_video(addr):
     os.remove(os.path.join(parent_dir, default_filename))
 
 def download_videos(addr):
-    p_list = Playlist(addr)
+    try:
+        p_list = Playlist(addr)
+    # 'list' 문자열을 찾지못하면 에러
+    except KeyError:
+        print('올바른 주소값을 입력해주십시오')
+        return
+
     p_list.download_all('./mp3')
     print('mp4 파일 다운로드를 완료하였습니다.')
     index = 0
+    list_len = len(str(len(p_list)))
+
     for link in p_list.parse_links():
         index += 1
+        index_str = len(str(index))
+        if index_str<list_len:
+            index = '0'*(list_len-index_str)+str(index)
+        else:
+            index = str(index)
+
         m_addr = 'youtube.com'+link
         yt = YouTube(m_addr).streams.filter(mime_type="audio/mp4").first()
         parent_dir = './mp3'
-        default_filename = str(index)+yt.default_filename 
+        default_filename = index+yt.default_filename 
         new_filename = os.path.splitext(default_filename)[0]+'.mp3'
         print(f'새로운 파일의 이름은 {new_filename}입니다.')
         
@@ -56,18 +73,21 @@ def download_videos(addr):
         '-vn',
         os.path.join(parent_dir, new_filename)
         ])
-        
+        index = int(index) 
         print('mp3 파일 다운로드를 완료하였습니다. mp4 파일을 삭제합니다.')
         os.remove(os.path.join(parent_dir, default_filename))
     
 
 choose_num = input("재생목록이면 1번 단일 비디오면 2번 : ")
 addr = input("주소를 입력하세요 : ")
+print('** 플레이리스트나 영상이 비공개상태거나 비어있다면 정상적으로 작동하지 않을 수 있습니다.')
 
 if choose_num=='1':
     download_videos(addr)
-else:
+elif choose_num=='2':
     download_video(addr)
+else:
+    print('1번과 2번중에 선택해주십시오')
 
 
 
